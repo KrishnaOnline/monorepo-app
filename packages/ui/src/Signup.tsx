86a104aeab87e0677112c1d222d0ab8bb3e4a92b
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-05-09 19:21:47 +0530

    monorepo setup

---
+++ packages/ui/src/Signup.tsx
@@ -0,0 +1,9 @@
+export const Signup = () => {
+    return (
+        <div>
+            <input type="text" placeholder="Email"></input>
+            <input type="password" placeholder="Password"></input>
+            <button>Submit</button>
+        </div>
+    )
+}
